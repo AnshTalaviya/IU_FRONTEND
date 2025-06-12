e2de8f2dbc7829d9efb006fe8415f9cc1dd682a1
Ansh Talaviya <159028042+AnshTalaviya@users.noreply.github.com>
2025-06-12 16:13:04 +0530

    Update TwoWheelerCard.jsx

--- src/components/DeliveryPages/BikeDeliveryPage/TwoWheelerCard.jsx
+++ src/components/DeliveryPages/BikeDeliveryPage/TwoWheelerCard.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { FaWeightHanging, FaTimes, FaMapMarkerAlt, FaPhoneAlt, FaUser } from 'react-icons/fa';
 import { motion } from 'framer-motion';
-// import { FaBox, FaFlask, FaAppleAlt, FaWeightHanging, FaTimes } from "react-icons/fa";
+import { FaBox, FaFlask, FaAppleAlt, FaWeightHanging, FaTimes } from "react-icons/fa";
 
 
 const TwoWheelerCard = () => {
